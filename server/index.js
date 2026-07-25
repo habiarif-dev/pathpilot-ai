bb0a8688aba3098afaee49b5d9732e4c4b261d60
habiarif-dev <habibaarif009@gmail.com>
2026-07-25 19:08:58 +0500

    Prepare backend for deployment

--- server/index.js
+++ server/index.js
@@ -122,7 +122,7 @@ app.use((error, req, res, next) => {
 
 const PORT = Number(process.env.PORT) || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(
     `🚀 Server running on http://localhost:${PORT}`
   );
